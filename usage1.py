1589a84b96ebcdb68c9f2f8fddbf5379c6dd6d3c
Su Yang <soulteary@gmail.com>
2024-06-27 14:24:47 +0800

    chore: add comment

--- usage1.py
+++ usage1.py
@@ -1,6 +1,7 @@
 import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModel
 
+# 定义输入内容
 input_texts = [
     "天气好热，哪里有卖冰棍的",
     "今天好冷，该多穿两件",
@@ -8,20 +9,23 @@ input_texts = [
     "冬天"
 ]
 
-# "thenlper/gte-base-zh"
+# 指定预训练模型，在线模型 "thenlper/gte-base-zh"，此处使用本地目录中的模型
 model_id = "./thenlper/gte-small"
 
+# 加载分词器和模型
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModel.from_pretrained(model_id)
 
-# Tokenize the input texts
+# 对输入内容进行分词、编码
 batch_dict = tokenizer(input_texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
 
+# 获取 Embeddings
 outputs = model(**batch_dict)
 embeddings = outputs.last_hidden_state[:, 0]
  
-# (Optionally) normalize embeddings
+# 标准化 embeddings，使用 L2 归一化，使其长度为 1
 embeddings = F.normalize(embeddings, p=2, dim=1)
+# 计算相似度
 scores = (embeddings[:1] @ embeddings[1:].T) * 100
+# 打印结果分数
 print(scores.tolist())
-
